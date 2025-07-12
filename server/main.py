c7af9e80b22296b14d893466b8687243a93fe60e
kaustubh05-prog <kaustubhmukdam22@gmail.com>
2025-07-12 13:27:38 +0530

    Login page with integration

--- server/main.py
+++ server/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 from server.routes.auth import auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+#Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["http://127.0.0.1:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(auth_router, prefix="/auth")
